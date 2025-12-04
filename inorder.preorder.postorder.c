6ec4dcd7324a20cf9479e6cd990a4af65a23e2b4
srikiran01 <srikiran0812@gmail.com>
2025-12-04 10:30:11 +0530

    Add files via upload

---
+++ inorder.preorder.postorder.c
@@ -0,0 +1,77 @@
+
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node
+{
+	int data;
+	struct node* left;
+	struct node* right;
+};
+
+struct node* createnode(int value)
+{
+	struct node* newnode=(struct node*)malloc(sizeof(struct node));
+	newnode->data=value;
+	newnode->left=NULL;
+	newnode->right=NULL;
+	return newnode;
+}
+
+//inorder traveral (left?root?right)
+void inorder(struct node* root)
+{
+	if(root!=NULL)
+	{
+		inorder(root->left);
+		printf("%d",root->data);
+		inorder(root->right);
+	}
+}
+
+//preorder traversal(root?left?right)
+void preorder(struct node* root)
+{
+	if(root!=NULL)
+	{
+		printf("%d",root->data);
+		preorder(root->left);
+		preorder(root->right);	
+	}
+}
+
+//postorder traversal (left?right?root)
+void postorder(struct node* root)
+{
+	if(root!=NULL)
+	{
+		postorder(root->left);
+		postorder(root->right);
+		printf("%d",root->data);
+	}
+}
+
+int main()
+{
+	struct node* root=createnode(4);
+	root->left=createnode(2);
+	root->right=createnode(6);
+	root->left->left=createnode(1);
+	root->left->right=createnode(3);
+	root->right->left=createnode(5);
+	root->right->right=createnode(7);
+	
+	printf("Inorder traversal: ");
+	inorder(root);
+	printf("\n");
+	
+	printf("Preorder traversal: ");
+	preorder(root);
+	printf("\n");
+	
+	printf("Postorder traversal: ");
+	postorder(root);
+	printf("\n");
+	
+	return 0;
+}
